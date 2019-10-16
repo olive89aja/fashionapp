cb0d1f98088d58dd7c31fa5bee46a245e311fa80
Olivier Auberger <50233804+olive89aja@users.noreply.github.com>
2019-10-16 18:16:53 -0400

    Add files via upload

---
+++ fashionappli/fashionapp.js
@@ -0,0 +1,15 @@
+$(".nodress").click(function() {
+
+    $("img").hide();
+    //$("img").slideToggle();
+    //$("p1").hide();
+
+});
+
+$(".moredress").click(function() {
+
+    $(".newdress").show();
+    //$("img").slideToggle();
+    //$("p1").hide();
+
+});
