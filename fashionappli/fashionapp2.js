35fb3b55d3770271291a76371b00b405feeee03b
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-11-04 21:28:15 -0600

    Created GET routes

---
+++ fashionappli/fashionapp2.js
@@ -0,0 +1,6 @@
+//Dresses GET https://api.imgur.com/3/account/oliviera89/album/e4KiFnc
+//Bottoms  GET https://api.imgur.com/3/account/oliviera89/album/GE3jLD5
+//Tops  GET https://api.imgur.com/3/account/oliviera89/album/IJ99jEi
+//Shoes GET https://api.imgur.com/3/account/oliviera89/album/GGDAEiB
+
+
