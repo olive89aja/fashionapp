23c128c347a8723e7df8d668be99b5d144856659
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-11-06 11:12:08 -0600

    Created four pages for four different categories of clothes

--- fashionappli/fashionapp2.js
+++ fashionappli/fashionapp2.js
@@ -1,6 +1,8 @@
+
+//Bottoms/Pants  GET https://api.imgur.com/3/account/oliviera89/album/GE3jLD5
 //Dresses GET https://api.imgur.com/3/account/oliviera89/album/e4KiFnc
-//Bottoms  GET https://api.imgur.com/3/account/oliviera89/album/GE3jLD5
-//Tops  GET https://api.imgur.com/3/account/oliviera89/album/IJ99jEi
 //Shoes GET https://api.imgur.com/3/account/oliviera89/album/GGDAEiB
+//Tops  GET https://api.imgur.com/3/account/oliviera89/album/IJ99jEi
+
 
 
