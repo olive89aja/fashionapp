6785d6f3d5fd76a32b080c349b24c08d71915d30
BrittGall <crlnfllr@gmail.com>
2019-11-06 01:07:44 -0600

    defined routes for images

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,65 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
+  // Get all clothes
+  app.get("/api/browse", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+  //only loads tops/shirts on browse page
+  app.get("/tops", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
-      res.json(dbExample);
+      res.render("example", {
+        example: dbExample
+      });
     });
   });
-};
+  //only loads bottoms:pants, skirts, shorts on browse page
+  app.get("/bottoms", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+  //only loads dresses on browse page
+  app.get("/dresses", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+  //only loads shoes on browse page
+  app.get("/shoes", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+//   // Create a new example
+//   app.post("/api/examples", function(req, res) {
+//     db.Example.create(req.body).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+
+//   // Delete an example by id
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Example.destroy({ where: { id: req.params.id } }).then(function(
+//       dbExample
+//     ) {
+//       res.json(dbExample);
+//     });
+//   });
+// };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
+  // Load index page/home
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
@@ -11,8 +11,68 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  // Loads all clothes on browse page
+  app.get("/browse", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+  //only loads tops/shirts on browse page
+  app.get("/tops", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+  //only loads bottoms:pants, skirts, shorts on browse page
+  app.get("/bottoms", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+  //only loads dresses on browse page
+  app.get("/dresses", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+  //only loads shoes on browse page
+  app.get("/shoes", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+  //loads saved clothing items
+  app.get("/closet", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+  //loads saved outfits
+  app.get("/outfit", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
