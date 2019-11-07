9a45e3be3064efe316112fe69abfada53a0226fa
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-11-06 20:03:09 -0600

    files updated

--- fashionappli/fashionapp2.js
+++
@@ -1,8 +0,0 @@
-
-//Bottoms/Pants  GET https://api.imgur.com/3/account/oliviera89/album/GE3jLD5
-//Dresses GET https://api.imgur.com/3/account/oliviera89/album/e4KiFnc
-//Shoes GET https://api.imgur.com/3/account/oliviera89/album/GGDAEiB
-//Tops  GET https://api.imgur.com/3/account/oliviera89/album/IJ99jEi
-
-
-
