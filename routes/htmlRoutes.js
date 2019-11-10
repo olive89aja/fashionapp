8497753b8598b6ad1540a3bb1096958f518efc8e
BrittGall <crlnfllr@gmail.com>
2019-11-09 18:50:18 -0600

    renamed 2 hbs files, required html routes sever

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,13 @@
 var db = require("../models");
-
+var path = require("path");
 module.exports = function(app) {
-  // Load index page
+  // // LOAD HOME PAGE WITH HTML
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../testing html/test_home.html"));
+  // });
+  // LOAD HOME PAGE WITH HANDLEBARS
   app.get("/", function(req, res) {
-    db.Outfit.findAll({}).then(function(dbOutfits) {
-      res.render("index", {
-        msg: "Swiftly, your digital closet",
-        outfits: dbOutfits
-      });
-    });
+    res.render("index");
   });
 
   // Load closet page where the outfits are made
@@ -35,40 +34,33 @@ module.exports = function(app) {
 
   // Load comment page and pass in an example by id
   app.get("/comments", function(req, res) {
-    db.Comment.findOne({ where: { id: req.params.id } }).then(function(dbComment) {
+    db.Comment.findOne({ where: { id: req.params.id } }).then(function(
+      dbComment
+    ) {
       res.render("comments", {
         comment: dbComment
-      }
+      });
     });
   });
-      
+
   // Load index page/home
 
-  app.get("/index", function(req, res) {
-    res.render("index", data);
-  });
-
   app.get("/browse", function(req, res) {
-    db.outfitsDB
-      .findOne({ where: { id: req.params.id } })
-      .then(function(outfitsDB) {
-        res.render("browse", {
-          example: outfitsDB
-        });
-      });
+    db.outfitsDB.findAll({}).then(function() {
+      res.render("browse");
+    });
   });
 
   app.get("/closet", function(req, res) {
     res.render("closet", data);
   });
-      
+
   app.get("/outfits", function(req, res) {
     res.render("outfits", data);
   });
-      
+
   // Render 404 page
   app.get("*", function(req, res) {
     res.render("404");
   });
-
 };
